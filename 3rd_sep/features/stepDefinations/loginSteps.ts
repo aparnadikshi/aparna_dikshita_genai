59ff92be926cadafd727c443341ef1b62a4b71d9
aparnadikshi <132121802+aparnadikshi@users.noreply.github.com>
2026-09-05 22:48:43 +0530

    Add login steps for Cucumber tests

---
+++ 3rd_sep/features/stepDefinations/loginSteps.ts
@@ -0,0 +1,16 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+
+Given('User opens application', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('User enters credentials', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('User should login successfully', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
